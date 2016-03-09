f40c7b291fc81360f6d542ab1055d2b2d6a2d34d
Aditya Gudibanda <adityagudibanda1995@gmail.com>
2016-03-08 19:44:49 -0500

    Added files via upload

--- midterm.py
+++ midterm.py
@@ -1,14 +1,4 @@
-n = raw_input("How many equations do you want?\n")
-
-m = []
-a = []
-b = []
-for i in range(0,int(n)):
-	print "Equation", i+1
-	a.append(raw_input("Enter the value of a\n"))
-	b.append(raw_input("Enter the value of b\n"))
-	m.append(raw_input("Enter the value of m\n"))
-
+#euclidean algorithm to calculate GCD of a and m
 def euclidgcd(a, m):
 	a = a % m
 	if a == 0:
@@ -18,7 +8,7 @@ def euclidgcd(a, m):
 		return (d, x - (m // a) * y, y)
 
 
-
+#calculates multiplicative inverse of a modulo m
 def multiplicativeinverse(a, m):
 	a = a % m
 	d, x, y = euclidgcd(a, m)
@@ -26,26 +16,6 @@ def multiplicativeinverse(a, m):
 		return 0;
 	else:
 		return x % m
-
-print multiplicativeinverse(1, 1)
-print multiplicativeinverse(6, 6)
-print multiplicativeinverse(2, 6)
-
-#def primefactorization(n):
-#	output = []
-#	i = 2
-#	k = 1
-#	while n != 1:
-#		k = 1;
-#		if n % i == 0:
-#			while n % i == 0:
-#				n = n/i
-#				k = k * i
-#			i = 2
-#		i = i + 1
-#		if k != 1:
-#			output.append(k)
-#	return output
 
 #Gives a list of all primes in the factorization of n
 def allprimesin(n):
@@ -69,124 +39,34 @@ def vp(p, n):
 		output = output + 1
 	return output
 
+
+#outputs a^b
 def power(a, b):
 	output = 1;
 	for i in range(0,b):
 		output = output*a;
 	return output
 
-
-#print allprimesin(12)
-#print vp(2, 100)
-#print power(2, 5)
-
-#outputs the max of a and b
-def maximumof(a, b):
-	if a > b:
-		return a
-	else:
-		return b
-
-#print maximumof(100, 120)
-
-#print a
-#print b
-#print m
-
-print "Here are your equations:"
-for i in range(0, int(n)):
-	print a[i],"x =",b[i]," ( mod",m[i],")"
-
-
-for i,j in enumerate(m):
-	if int(m[i]) == 0:
-		print "Modulo cannot be zero"
-		quit()
-	if int(a[i]) == 0 and int(b[i]) % int(m[i]) != 0:
-		print "Equation", i+1, "is unsolvable"
-		quit()
-
-ainverses = []
-
-for i,j in enumerate(a):
-	ainv = multiplicativeinverse(int(j), int(m[i]))
-	if ainv == 0:
-		GCD,irrel, irrel2 = euclidgcd(int(j), int(m[i]))
-		print "GCD is", GCD
-		#print GCD
-		if (int(b[i]) % int(m[i])) % GCD == 0:
-			a[i] = int(a[i])/GCD
-			b[i] = int(b[i])/GCD
-			m[i] = int(m[i])/GCD
-			print "a", a[i], "m", m[i]
-			ainv = multiplicativeinverse(int(a[i]), int(m[i]))
-			#if ainv == 0:
-		#		print "case 1"
-	#			print "Equation", i+1, "is unsolvable"
-#				quit()
-		else:
-			print "case 2"
-			print "Equation", i+1, "is unsolvable"
-			quit()
-	ainverses.append(ainv)
-
-
-#foundaninverse = 0;
-#for i,j in enumerate(m):
-#	foundaninverse = 0;
-#	for k in range(0, int(j)):
-#		if k * int(a[i]) % int(j) == 1:
-#			ainverses.append(k)
-#			foundaninverse = 1
-#	if foundaninverse == 0:
-#		print "Equation", i, "is unsolvable"
-#		quit()
-#print ainverses
-newb = []
-for i,j in enumerate(b):
-	newb.append(int(j) * ainverses[i] % int(m[i]))
-
-
-for i in range(0,int(n)):
-	for j in range(0,int(n)):
-		GCD, irrel, irrel2 = euclidgcd(int(m[i]), int(m[j]))
-		if ((int(newb[i]) - int(newb[j])) % GCD != 0):
-			print "Equations", i+1, "and", j+1, "are not compatible"
-			quit()
-
-
 def solver(b, m):
-	print "b is", b
-	print "m is", m
-	#print len(m)
-	#print "there"
 	if len(m) == 1:
+		#if there is only one equation left and the modulo is 1, then the solution is all integers
 		if int(m[0]) == 1:
 			print "Solution is all integers"
 			quit()
 		else:
+			#otherwise just print the only solution
 			print "Solution"
 			print "x =", int(b[0]) % int(m[0]), "( mod", int(m[0]), ")"
 			quit()
 
-
+	#if the modulo of the first equation is one, then just remove that equation and solve the remaining equations
 	if int(m[0]) == 1:
 		m = m[1:]
 		b = b[1:]
 		solver(b, m)
-	#productofm = 1
-	#for i in m:
-#		productofm = productofm * int(i)
 
-	#newm = []
-	#for i in m:
-	#	newm.append(productofm/int(i))
-	#y = []
-	#for i,j in enumerate(m):
-	#	for k in range(0,int(j)):
-	#		if k*int(newm[i]) % int(j) == 1:
-	#			y.append(k)
 	GCD, irrel, irrel2 = euclidgcd(int(m[0]), int(m[1]))
+	#if the GCD is 1
 	if GCD == 1:
 		mcut = m[0:2]
 		bcut = b[0:2]
@@ -195,10 +75,7 @@ def solver(b, m):
 		newm.append(m[1])
 		newm.append(m[0])
 		y = []
-	#	for i,j in enumerate(mcut):
-	#		for k in range(0, int(j)):
-	#			if k * int(newm[i]) % int(j) == 1:
-	#				y.append(k)
+
 		for i,j in enumerate(mcut):
 			yval = multiplicativeinverse(int(newm[i]), int(j))
 			y.append(yval)
@@ -207,6 +84,7 @@ def solver(b, m):
 		for i in range(0, 2):
 			output = output + int(b[i]) * int(y[i]) * int(newm[i])
 
+		#assign the results to index 1 of m and b, and cut off the index 0 elements, then solve from here.
 		m[1] = int(m[1])*int(m[0]);
 		b[1] = output % int(m[1])
 
@@ -215,21 +93,9 @@ def solver(b, m):
 
 		solver(b, m)
 	else:
-		#m.append(GCD)
-		#b.append(b[0])
-		#m.append(int(m[0])/GCD)
-		#b.append(b[0])
-		#m.append(int(m[1])/GCD)
-		#b.append(b[1])
-		#b = b[2:]
-		#m = m[2:]
-		#solver(b,m)
 		GCDprimes = allprimesin(int(m[0]) * int(m[1]))
-		#print "Gcdprimes is", GCDprimes
-		#m0factorization = primefactorization(int(m[0]))
-		#m1factorization = primefactorization(int(m[1]))
 
-
+		#for each prime in either m[0] or m[1], only the highest power matters, so choose whichever has the highest power of that prime and then append it to the end
 		for i in GCDprimes:
 			m0power = vp(i, int(m[0]))
 			m1power = vp(i, int(m[1]))
@@ -240,29 +106,83 @@ def solver(b, m):
 				m.append(power(i, m1power))
 				b.append(int(b[1]))
 
+		#cut off the first two elements and then solve from there
 		m = m[2:]
 		b = b[2:]
-		solver(b, m)		
+		solver(b, m)
+
+#inputs the number of equations
+n = raw_input("How many equations do you want?\n")
+
+m = []
+a = []
+b = []
+
+#inputs the values of ai, bi, and mi
+for i in range(0,int(n)):
+	print "Equation", i+1
+	a.append(raw_input("Enter the value of a\n"))
+	b.append(raw_input("Enter the value of b\n"))
+	m.append(raw_input("Enter the value of m\n"))
 
 
+#prints all equations
+print "Here are your equations:"
+for i in range(0, int(n)):
+	print a[i],"x =",b[i]," ( mod",m[i],")"
 
 
-	#print a
-	#print b
-	#print m
-	#print newm
-	#print y
+#first, cleans the numbers
+for i,j in enumerate(m):
+	#makes negative modulo's positive
+	if int(m[i]) < 0:
+		m[i] = -1 * int(m[i])
+	#quits if one of the modulo's is zero
+	if int(m[i]) == 0:
+		print "Modulo cannot be zero"
+		quit()
+	#quits if a modulo m is 0 and b modulo m is not zero
+	if int(a[i]) % int(m[i]) == 0 and int(b[i]) % int(m[i]) != 0:
+		print "Equation", i+1, "is unsolvable"
+		quit()
+	#converts a and b to their canonical residue modulo m, between 0 and m-1
+	b[i] = int(b[i]) % int(m[i])
+	a[i] = int(a[i]) % int(m[i])
+
+ainverses = []
+
+#creates the list of inverses of a modulo m
+for i,j in enumerate(a):
+	ainv = multiplicativeinverse(int(j), int(m[i]))
+	#ainv = 0 if the multiplicative inverse does not exist
+	if ainv == 0:
+		GCD,irrel, irrel2 = euclidgcd(int(j), int(m[i]))
+		#divide everything by GCD. The case where m = 1 will be taken care of later.
+		if (int(b[i]) % int(m[i])) % GCD == 0:
+			a[i] = int(a[i])/GCD
+			b[i] = int(b[i])/GCD
+			m[i] = int(m[i])/GCD
+			ainv = multiplicativeinverse(int(a[i]), int(m[i]))
+		else:
+			print "Equation", i+1, "is unsolvable"
+			quit()
+	ainverses.append(ainv)
+
+newb = []
+#multiply by the inverses so we have a system where all the x's have coefficient one.
+for i,j in enumerate(b):
+	newb.append(int(j) * ainverses[i] % int(m[i]))
 
 
-	#output = 0;
+#checks if any two equations are incompatible, quits if any two are.
+for i in range(0,int(n)):
+	for j in range(0,int(n)):
+		GCD, irrel, irrel2 = euclidgcd(int(m[i]), int(m[j]))
+		if ((int(newb[i]) - int(newb[j])) % GCD != 0):
+			print "Equations", i+1, "and", j+1, "are not compatible"
+			quit()
 
-	#for i in range(0,int(n)):
-	#	output = output + int(b[i])*int(y[i])*int(newm[i])
-
-
-	#print "Solution:"
-	#print "x =", output % productofm, "( mod", productofm, ")"
-
+#calls the solver function
 solver(newb, m)
 
 
